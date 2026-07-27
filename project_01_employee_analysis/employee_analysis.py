c5e41b9fb473d3ab8d3daaf6738398ade8f76cb3
Ko Bhone <bhonem908@gmail.com>
2026-07-27 22:39:12 +0630

    feat: Create Employee Analysis Project

---
+++ project_01_employee_analysis/employee_analysis.py
@@ -0,0 +1,123 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class EmployeeAnalysisFramework:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.df = None
+
+    # Load Dataset
+    def load_data(self):
+        self.df = pd.read_csv(self.file_path)
+        print("\nDataset Loaded Successfully")
+
+    # Dataset Information
+    def data_information(self):
+        print("\n========== First 5 Rows ==========")
+        print(self.df.head())
+        print("\n========== Dataset Information ==========")
+        self.df.info()
+        print("\n========== Statistical Summary ==========")
+        print(self.df.describe())
+        print("\nShape :", self.df.shape)
+
+    # Data Cleaning
+    def clean_data(self):
+        print("\n========== Missing Values ==========")
+        print(self.df.isnull().sum())
+        print("\n========== Duplicate Rows ==========")
+        print(self.df.duplicated().sum())
+        self.df.drop_duplicates(inplace=True)
+
+    # NumPy Analysis
+    def numpy_analysis(self):
+        salary = self.df["Salary"].values
+        print("\n========== NumPy Analysis ==========")
+        print("Average Salary :", np.mean(salary))
+        print("Median Salary :", np.median(salary))
+        print("Standard Deviation :", np.std(salary))
+        self.df["Salary_Level"] = np.where(self.df["Salary"] >= 70000,"High Salary","Low Salary")
+        conditions = [self.df["Experience"] < 3, self.df["Experience"] <= 8, self.df["Experience"] > 8]
+        choices = ["Junior", "Mid Level", "Senior"]
+        self.df["Experience_Level"] = np.select(conditions, choices, default="Unknown")
+        print("Salary Level Added")
+        print("Experience Level Added")
+
+    # Feature Engineering
+    def feature_engineering(self):
+        self.df["Salary_Per_Year"] = (self.df["Salary"] / (self.df["Experience"] + 1))
+        self.df["Age_Group"] = pd.cut(self.df["Age"], bins=[0, 25, 35, 100], labels=["Young", "Adult", "Senior"])
+        print("\nFeature Engineering Completed")
+
+    # Pandas Analysis
+    def pandas_analysis(self):
+        print("\n========== Department Count ==========")
+        print(self.df["Department"].value_counts())
+        print("\n========== Average Salary ==========")
+        dept_salary = (self.df.groupby("Department")["Salary"].mean().sort_values(ascending=False))
+        print(dept_salary)
+        print("\n========== Top 5 Salary ==========")
+        print(self.df.sort_values(by="Salary", ascending=False).head(5))
+        print("\n========== IT Employees ==========")
+        print(self.df[self.df["Department"] == "IT"])
+
+    # Visualization
+    def visualization(self):
+        output = "outputs"
+        os.makedirs(output, exist_ok=True)
+        sns.set_theme()
+        # Bar Chart
+        plt.figure(figsize=(8,5))
+        (self.df.groupby("Department")["Salary"].mean().plot(kind="bar"))
+        plt.title("Average Salary By Department")
+        plt.ylabel("Salary")
+        plt.tight_layout()
+        plt.savefig(f"{output}/department_salary.png")
+        plt.close()
+        # Pie Chart
+        plt.figure(figsize=(7,7))
+        self.df["Department"].value_counts().plot(kind="pie", autopct="%1.1f%%")
+        plt.title("Department Distribution")
+        plt.ylabel("")
+        plt.savefig(f"{output}/department_pie.png")
+        plt.close()
+        # Histogram
+        plt.figure(figsize=(8,5))
+        sns.histplot(self.df["Salary"], bins=8, kde=True)
+        plt.title("Salary Distribution")
+        plt.savefig(f"{output}/salary_distribution.png")
+        plt.close()
+        # Box Plot
+        plt.figure(figsize=(8,5))
+        sns.boxplot(y=self.df["Salary"])
+        plt.title("Salary Outlier Detection")
+        plt.savefig(f"{output}/salary_boxplot.png")
+        plt.close()
+        # Scatter Plot
+        plt.figure(figsize=(8,5))
+        sns.scatterplot(data=self.df, x="Experience", y="Salary", hue="Department")
+        plt.title("Experience vs Salary")
+        plt.savefig(f"{output}/experience_salary.png")
+        plt.close()
+        print("\nAll Charts Saved Successfully")
+
+    # Complete Pipeline
+    def run(self):
+        self.load_data()
+        self.data_information()
+        self.clean_data()
+        self.numpy_analysis()
+        self.feature_engineering()
+        self.pandas_analysis()
+        self.visualization()
+        print("\nEmployee Analysis Completed!")
+
+def main():
+    framework = EmployeeAnalysisFramework("datasets/employee_data.csv")
+    framework.run()
+
+if __name__ == "__main__":
+    main()
